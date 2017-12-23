7ee46e48c8501032d349011a586f41d1019ee33b
Cory Reed <swashcap@gmail.com>
2017-12-23 14:57:56 -0800

    refactor(client): remove hard-coded elements

--- client/components/Cameras.js
+++ client/components/Cameras.js
@@ -2,36 +2,40 @@ import preact from 'preact' // eslint-disable-line no-unused-vars
 
 import './Cameras.css'
 
-const Cameras = () => (
-  <div className='Cameras'>
-    <a href="http://www.kgw.com/weather/live-cameras/portland-skyline-wells-fargo">
-      <img
-        alt="Wells Fargo"
-        src="http://cdn.tegna-media.com/kgw/weather/wellsfargo.jpg"
-      />
-      <p>Downtown Portland</p>
-    </a>
-    <a href='https://tripcheck.com/DynamicReports/Report/Cameras'>
-      <img
-        alt='US-26 at Gov Camp'
-        src="https://tripcheck.com/RoadCams/cams/US26 at Govt Camp Lp Rd_pid1776.JPG?rand=1513974775609" />
-      <p>Government Camp</p>
-    </a>
-    <a href='http://www.oregonsurf.com/pages/cams.html'>
-      <img
-        alt='Roads End, Lincoln City'
-        src='http://www.lcsurfshop.com/webcam32.jpg'
-      />
-      <p>Lincoln City</p>
-    </a>
-    <a href='https://www.timberlinelodge.com/conditions'>
-      <img
-        alt='Timerline Lodge'
-        src='https://www.timberlinelodge.com/snowcameras/lodge.jpg?nocache=1513976335'
-      />
-      <p>Timerline Lodge</p>
-    </a>
-  </div>
-)
+const Cameras = () => {
+  const now = Date.now()
+  const items = [{
+    alt: 'Wells Fargo',
+    link: 'http://www.kgw.com/weather/live-cameras/portland-skyline-wells-fargo',
+    name: 'Downtown Portland',
+    src: 'http://cdn.tegna-media.com/kgw/weather/wellsfargo.jpg'
+  }, {
+    alt: 'US-26 at Gov Camp',
+    link: 'https://tripcheck.com/DynamicReports/Report/Cameras',
+    name: 'Government Camp',
+    src: `https://tripcheck.com/RoadCams/cams/US26%20at%20Govn%20Camp%20Maint_pid1899.JPG?rand=${now}`
+  }, {
+    alt: 'Roads End, Lincoln City',
+    link: 'http://www.oregonsurf.com/pages/cams.html',
+    name: 'Lincoln City',
+    src: 'http://www.lcsurfshop.com/webcam32.jpg'
+  }, {
+    alt: 'Timerline Lodge',
+    link: 'https://www.timberlinelodge.com/conditions',
+    name: 'Timerline Lodge',
+    src: `https://www.timberlinelodge.com/snowcameras/lodge.jpg?nocache=${Math.floor(now / 1000)}`
+  }]
+
+  return (
+    <div className='Cameras'>
+      {items.map(({ alt, link, name, src }) => (
+        <a href={link} key={link}>
+          <img alt={alt} src={src} />
+          <p>{name}</p>
+        </a>
+      ))}
+    </div>
+  )
+}
 
 export default Cameras
