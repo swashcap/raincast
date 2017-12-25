8cd55875ed4f8e6a5319ca7291b974b526f66d93
Cory Reed <swashcap@gmail.com>
2017-12-25 12:16:25 -0800

    fix(pkg): fix react-router redux integration

--- src/render/components/Root.js
+++ src/render/components/Root.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const PropTypes = require('prop-types')
 const { ConnectedRouter } = require('react-router-redux')
-const { Route } = require('react-router')
+const { Route } = require('react-router-dom')
 const { Provider } = require('react-redux')
 
 const Admin = require('./Admin.js')
