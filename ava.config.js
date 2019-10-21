430ccd8a41bf0800dc73ddcab2965179355b5d6b
Cory Reed <swashcap@gmail.com>
2019-10-21 00:54:08 -0700

    fix: correct test config

---
+++ ava.config.js
@@ -0,0 +1,9 @@
+export default {
+  require: [
+    /**
+     * Compile files in src/
+     * {@link https://github.com/avajs/ava/blob/master/docs/recipes/babel.md#compile-sources}
+     */
+    '@babel/register'
+  ]
+}
