88db90748a8d709d50a4f225fc28dfdecfb2f91d
Cory Reed <swashcap@gmail.com>
2019-10-21 00:29:50 -0700

    feat: stub responses during development

--- src/main/services/dark-sky.js
+++ src/main/services/dark-sky.js
@@ -5,8 +5,12 @@ const darkSky = ({
   // TODO: Move location (PDX) to config
   latitude = 45.512794,
   longitude = -122.679565
-}) =>
-  request({
+}) => {
+  if (process.env.NODE_ENV === 'development') {
+    return Promise.resolve(require('../../../mocks/darksky.json'))
+  }
+
+  return request({
     headers: {
       'Accept-Encoding': 'gzip,deflate'
     },
@@ -14,5 +18,6 @@ const darkSky = ({
     url: `https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}`
   })
     .then((response) => JSON.parse(response))
+}
 
 module.exports = darkSky
--- src/main/services/weather-alerts.js
+++ src/main/services/weather-alerts.js
@@ -1,21 +1,29 @@
+const fs = require('fs')
+const path = require('path')
 const { promisify } = require('util')
 const { parseString } = require('xml2js')
 
 const request = require('../utils/request.js')
 
-const parseXml = promisify(parseString)
+const mapResponse = (response) => promisify(parseString)(response)
+  .then(({ feed }) => feed.entry ? feed.entry : [])
 
 const weatherAlerts = ({
   x = 'ORZ006',
   y = '0'
-}) =>
-  request({
+}) => {
+  if (process.env.NODE_ENV === 'development') {
+    return promisify(fs.readFile)(path.resolve(__dirname, '../../../mocks/alerts.xml'), 'utf8')
+      .then(mapResponse)
+  }
+
+  return request({
     headers: {
       'User-Agent': 'request'
     },
     url: `https://alerts.weather.gov/cap/wwaatmget.php?x=${x}&y=${y}`
   })
-    .then(parseXml)
-    .then(({ feed }) => feed.entry ? feed.entry : [])
+    .then(mapResponse)
+}
 
 module.exports = weatherAlerts
